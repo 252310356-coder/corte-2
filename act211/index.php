02046f7db817fd287d209c5cb04678dc627da99d
Otoniel <252310356@itslerdo.edu.mx>
2026-02-19 10:32:56 -0600

    Creacion de la actividad 2.1.1, con la clase automovil, el index, varios objetos mas y el metodo para cambiar el color del autmovil

---
+++ act211/index.php
@@ -0,0 +1,22 @@
+<?php
+require 'Automovil.php'; // Incluimos la clase Automovil
+// Crear instancias de Automovil
+$auto1 = new Automovil("Rojo", "Toyota", 4, false);
+$auto2 = new Automovil("Negro", "Ford", 2, true);
+$auto3 = new Automovil("Azul", "Honda", 4, false);
+$auto4 = new Automovil("Blanco", "Chevrolet", 2, true);
+// Mostrar información de los autos
+echo "<h2>Información del Automóvil 1</h2>";
+echo $auto1->mostrarDatos();
+echo "<h2>Información del Automóvil 2</h2>";
+echo $auto2->mostrarDatos();
+echo "<h2>Información del Automóvil 3</h2>";
+echo $auto3->mostrarDatos();
+echo "<h2>Información del Automóvil 4</h2>";
+echo $auto4->mostrarDatos();
+
+echo "<h2>Información del Automóvil 1 después de cambiar el color</h2>";
+echo $auto1->cambio_color("Verde"); // Cambiamos el color del auto1
+echo "<h2>Información del Automóvil 3 después de cambiar el color</h2>";
+echo $auto3->cambio_color("Amarillo"); // Cambiamos el color del auto3
+?>
